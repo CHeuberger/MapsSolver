378d47ccba66e1ae4672ab5be50b3ae1e5dbee25
Carlos Heuberger <fursgyv.cfh@dfgh.net>
2017-05-29 18:16:22 +0000

    trying to fidn border

--- src/java/cfh/maps/ImagePanel.java
+++ src/java/cfh/maps/ImagePanel.java
@@ -1,8 +1,9 @@
 package cfh.maps;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -10,27 +11,50 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class ImagePanel extends JPanel {
 
-    private Image image = null;
+    private BufferedImage image = null;
+    private BufferedImage overlay = null;
     
     
     ImagePanel() {
     }
     
-    public void setImage(Image image) {
+    public void setImage(BufferedImage image) {
         this.image = image;
+        overlay = null;
+        revalidate();
         repaint();
     }
     
+    public void setOriginalSize(int width, int height) {
+        super.setPreferredSize(new Dimension(width+6, height+6));
+    }
+    
+    public void setOverlay(BufferedImage overlay) {
+        this.overlay = overlay;
+    }
+    
+    public BufferedImage getOverlay() {
+        return overlay;
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         
-        if (image != null && image.getHeight(this) != -1) {
+        if (image != null) {
             Color tmp = g.getColor();
             try {
                 g.setColor(Color.BLACK);
-                g.fillRect(0, 0, image.getWidth(null)+4, image.getHeight(null)+4);
-                g.drawImage(image, 2, 2, this);
+                Dimension size = getPreferredSize();
+                int w = image.getWidth();
+                int h = image.getHeight();
+                int x = (size.width - w) / 2;
+                int y = (size.height - h) / 2;
+                g.fillRect(x-2, y-2, w+4, h+4);
+                g.drawImage(image, x, y, this);
+                if (overlay != null) {
+                    g.drawImage(overlay, x, y, this);
+                }
             } finally {
                 g.setColor(tmp);
             }
--- src/java/cfh/maps/MainPanel.java
+++ src/java/cfh/maps/MainPanel.java
@@ -1,16 +1,21 @@
 package cfh.maps;
 
+import static java.lang.Math.*;
 import static javax.swing.JOptionPane.*;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.io.IOException;
 import java.util.function.Consumer;
 
@@ -19,17 +24,32 @@ import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
 
 
 @SuppressWarnings("serial")
 public class MainPanel extends JPanel {
     
+    enum State {
+        EMPTY, NEW, BORDER
+    }
+    
+    private static final int BORDER_DIST = 30;
+    
+    private State state;
+    
     private Action pasteAction;
+    private Action borderAction;
 
     private ImagePanel imagePanel;
-    private JTextField status;
+    private JTextField stateField;
+    private JTextField messageField;
+    
+    private BufferedImage originalImage = null;
+    private ColorModel colorModel = null;
 
 
     MainPanel() {
@@ -39,34 +59,52 @@ public class MainPanel extends JPanel {
         
         imagePanel = new ImagePanel();
         
-        status = new JTextField();
-        status.setEditable(false);
-        
         JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-        splitPane.setTopComponent(imagePanel);
+        JPanel ip = new JPanel();
+        ip.add(imagePanel);
+        splitPane.setTopComponent(new JScrollPane(ip));
+        
+        messageField = new JTextField("");
+        messageField.setEditable(false);
+        
+        stateField = new JTextField(6);
+        stateField.setEditable(false);
+        stateField.setHorizontalAlignment(JTextField.CENTER);
+        
+        JPanel statusPanel = new JPanel();
+        statusPanel.setLayout(new BorderLayout());
+        statusPanel.add(messageField, BorderLayout.CENTER);
+        statusPanel.add(stateField, BorderLayout.LINE_END);
         
         setPreferredSize(new Dimension(800, 600));
         setLayout(new BorderLayout());
         add(menubar, BorderLayout.PAGE_START);
         add(splitPane, BorderLayout.CENTER);
-        add(status, BorderLayout.PAGE_END);
+        add(statusPanel, BorderLayout.PAGE_END);
         
-        setStatus("");
+        setState(State.EMPTY);
+        setMessage("");
     }
 
     private void doPaste(ActionEvent ev) {
         try {
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             if (clipboard.isDataFlavorAvailable(DataFlavor.imageFlavor)) {
-                Image image;
+                Image img;
                 try {
-                    image = (Image) clipboard.getData(DataFlavor.imageFlavor);
+                    img = (Image) clipboard.getData(DataFlavor.imageFlavor);
                 } catch (UnsupportedFlavorException | IOException ex) {
                     report(ex);
                     return;
                 }
-                imagePanel.setImage(image);
-                setStatus("Loaded %d x %d", image.getWidth(null), image.getHeight(null));
+                originalImage = toBufferedImage(img);
+                colorModel = originalImage.getColorModel();
+                int w = originalImage.getWidth();
+                int h = originalImage.getHeight();
+                imagePanel.setImage(originalImage);
+                imagePanel.setOriginalSize(w, h);
+                setState(State.NEW);
+                setMessage("Loaded %d x %d", w, h);
             } else {
                 showMessageDialog(this, "no image to paste");
             }
@@ -76,8 +114,67 @@ public class MainPanel extends JPanel {
         }
     }
     
+    private void doBorder(ActionEvent ev) {
+        new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                if (originalImage == null)
+                    return null;
+                BufferedImage overlay = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+                imagePanel.setOverlay(overlay);
+                
+                // diagonal
+                int ref = metric(0, 0);
+                int x = 0;
+                int y = 0;
+                boolean found = false;
+                while (!found && ++x < originalImage.getWidth() && ++y < originalImage.getHeight()) {
+                    int m = metric(x, y);
+                    found = (abs(m-ref) > BORDER_DIST);
+                    overlay.setRGB(x, y, Color.RED.getRGB());
+                    imagePanel.repaint();
+                }
+                if (!found)
+                    return null;
+                
+                found = false;
+                while (!found && --x < originalImage.getWidth()) {
+                    int m = metric(x, y);
+                    found = (abs(m-ref) <= BORDER_DIST);
+                    overlay.setRGB(x, y, Color.RED.getRGB());
+                    imagePanel.repaint();
+                }
+                x += 1;
+ 
+                if (!found) {
+                    while (!found && --y < originalImage.getHeight()) {
+                        int m = metric(x, y);
+                        found = (abs(m-ref) <= BORDER_DIST);
+                        overlay.setRGB(x, y, Color.RED.getRGB());
+                        imagePanel.repaint();
+                    }
+                    y += 1;
+                }
+                
+                if (found) {
+                    Graphics2D og = overlay.createGraphics();
+                    og.setColor(Color.GREEN);
+                    og.drawLine(x-2, y-2, x+2, y+2);
+                    og.drawLine(x-2, y+2, x+2, y-2);
+                    imagePanel.repaint();
+                    og.dispose();
+                }
+                
+                // TODO Auto-generated method stub
+                return null;
+            }
+        }.execute();
+        // TODO
+    }
+    
     private void initActions() {
         pasteAction = makeAction("Paste", "Paste a new map", this::doPaste);
+        borderAction = makeAction("Border", "Find external border", this::doBorder);
     }
     
     private Action makeAction(String name, String tooltip, Consumer<ActionEvent> listener) {
@@ -95,21 +192,51 @@ public class MainPanel extends JPanel {
         JMenu fileMenu = new JMenu("File");
         fileMenu.add(pasteAction);
         
+        JMenu analyseMenu = new JMenu("Analyse");
+        analyseMenu.add(borderAction);
+        
         JMenuBar bar = new JMenuBar();
         bar.add(fileMenu);
+        bar.add(analyseMenu);
         
         return bar;
     }
     
-    private void setStatus(String format, Object... args) {
-        status.setText(String.format(format, args));
-        status.setBackground(null);
+    private BufferedImage toBufferedImage(Image img) {
+        if (img instanceof BufferedImage)
+            return (BufferedImage) img;
+        
+        BufferedImage result = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D gg = result.createGraphics();
+        gg.drawImage(img, 0, 0, null);
+        gg.dispose();
+
+        return result;
+    }
+    
+    private int metric(int x, int y) {
+        int rgb = originalImage.getRGB(x, y);
+        int r = colorModel.getRed(rgb);
+        int g = colorModel.getGreen(rgb);
+        int b = colorModel.getBlue(rgb);
+        return 2*r*r + 4*g*g + 3*b*b;
+    }
+    
+    private void setState(State state) {
+        this.state = state;
+        stateField.setText(state.toString());
+        borderAction.setEnabled(state != State.EMPTY);
+    }
+    
+    private void setMessage(String format, Object... args) {
+        messageField.setText(String.format(format, args));
+        messageField.setBackground(null);
     }
     
     private void report(Throwable ex) {
         ex.printStackTrace();
-        setStatus("%s: %s", ex.getClass().getSimpleName(), ex.getMessage());
-        status.setBackground(Color.RED);
+        setMessage("%s: %s", ex.getClass().getSimpleName(), ex.getMessage());
+        messageField.setBackground(Color.RED);
         showMessageDialog(this, ex.getMessage(), ex.getClass().getSimpleName(), ERROR_MESSAGE);
     }
 }
